7d2316d11b0471a4112f6696c4e2692c79d1a741
Ao Liu <18271390154@163.com>
2021-05-17 11:53:25 +0800

    update the name of implicit service

---
+++ app/src/main/java/com/example/ask/ui/intentSchemeUrl/intentSchemeUrlTestActivity.java
@@ -0,0 +1,21 @@
+package com.example.ask.ui.intentSchemeUrl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.ask.R;
+
+public class intentSchemeUrlTestActivity extends Activity {
+    TextView textView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intent_scheme_url_test);
+        textView = findViewById(R.id.intent_scheme_url_test_text_view);
+        textView.setText("本测试activity被成功调用\n利用成功!\n");
+
+    }
+}
